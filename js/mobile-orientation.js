189be45945b4593220a567d738aa5911bc970bcf
Serhieie <89160714+Serhieie@users.noreply.github.com>
2023-08-14 11:17:09 +0300

    modal-update

--- js/mobile-orientation.js
+++ js/mobile-orientation.js
@@ -3,23 +3,16 @@ function toggleModalOrientationClass(orientation) {
 
   if (modal) {
     if (orientation === "landscape") {
-      // Add the "landscape-mode" class
-      modal.classList.add("landscape-mode");
-    } else {
-      // Remove the "landscape-mode" class if it exists
       modal.classList.remove("landscape-mode");
+    } else {
+      modal.classList.add("landscape-mode");
     }
   }
 }
-
-// Function to handle orientation change
 function handleOrientationChange() {
   const orientation = window.innerWidth > window.innerHeight ? "landscape" : "portrait";
   toggleModalOrientationClass(orientation);
 }
 
-// Add event listener for orientation change
 window.addEventListener("orientationchange", handleOrientationChange);
-
-// Call the function initially to apply the class based on the initial orientation
 handleOrientationChange();
