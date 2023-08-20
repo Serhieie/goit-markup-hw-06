ab43767f8b606bd2eb70293a9074b02e692d0962
Serhieie <89160714+Serhieie@users.noreply.github.com>
2023-08-20 15:54:26 +0300

    big changes with mobile menu try 1

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,24 +1,28 @@
 (() => {
-  const mobileMenu = document.querySelector(".js-menu-container");
-  const openMenuBtn = document.querySelector(".js-open-menu");
-  const closeMenuBtn = document.querySelector(".js-close-menu");
+  const mobileMenu = document.querySelector(".menu-burger");
+  const openMenuBtn = document.querySelector(".menu-burger-btn");
+  const body = document.querySelector("body");
 
   const toggleMenu = () => {
     const isMenuOpen = openMenuBtn.getAttribute("aria-expanded") === "true" || false;
     openMenuBtn.setAttribute("aria-expanded", !isMenuOpen);
     mobileMenu.classList.toggle("is-open");
+    openMenuBtn.classList.toggle("is-open");
+    body.classList.toggle("no-scroll");
 
     const scrollLockMethod = !isMenuOpen ? "disableBodyScroll" : "enableBodyScroll";
-    bodyScrollLock[scrollLockMethod](document.body);
   };
 
   openMenuBtn.addEventListener("click", toggleMenu);
-  closeMenuBtn.addEventListener("click", toggleMenu);
 
+  // Закрываем мобильное меню на более широких экранах
+  // в случае изменения ориентации устройства.
   window.matchMedia("(min-width: 768px)").addEventListener("change", (e) => {
     if (!e.matches) return;
+
     mobileMenu.classList.remove("is-open");
+    openMenuBtn.classList.remove("is-open");
+    body.classList.remove("no-scroll");
     openMenuBtn.setAttribute("aria-expanded", false);
-    bodyScrollLock.enableBodyScroll(document.body);
   });
 })();
