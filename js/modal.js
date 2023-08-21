cdd9fb4ff8754b29730e85fedf4de8eb83815adb
Serhieie <89160714+Serhieie@users.noreply.github.com>
2023-08-21 09:26:44 +0300

    modal-js-upd

--- js/modal.js
+++ js/modal.js
@@ -8,6 +8,24 @@
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
+  const thxContainder = document.querySelector(".thanks-containder");
+const thxBtn = document.querySelector(".thanks-button")
+const form = document.querySelector(".modal-form")
+const heroContainer = document.querySelector(".hero-container")
+
+form.addEventListener("submit", (event) => {
+  thxContainder.classList.add("is-open");
+  refs.modal.classList.add("is-hidden")
+  heroContainer.classList.add("is-hidden")
+  event.preventDefault();
+})
+
+thxBtn.addEventListener("click", () => {
+  thxContainder.classList.remove("is-open");
+  heroContainer.classList.remove("is-hidden")
+  document.body.classList.remove("modal-open")
+});
+
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
     document.body.classList.toggle("modal-open");
