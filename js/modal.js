b21520916b51325330fddc7003ddde1927b2b2c6
Serhieie <89160714+Serhieie@users.noreply.github.com>
2023-08-21 10:16:28 +0300

    adwa

--- js/modal.js
+++ js/modal.js
@@ -3,28 +3,28 @@
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
+    form: document.querySelector(".modal-form"),
+    thxContainder: document.querySelector(".thanks-containder"),
+    thxBtn: document.querySelector(".thanks-button"),
+    heroContainer: document.querySelector(".hero-container")
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.form.addEventListener("submit", toggleThanksMsg);
+  refs.thxBtn.addEventListener("click", toggleThanksMsg);
+  refs.thxBtn.addEventListener("click", removeModalOpn);
 
-  const thxContainder = document.querySelector(".thanks-containder");
-const thxBtn = document.querySelector(".thanks-button")
-const form = document.querySelector(".modal-form")
-const heroContainer = document.querySelector(".hero-container")
-
-form.addEventListener("submit", (event) => {
-  thxContainder.classList.add("is-open");
-  refs.modal.classList.add("is-hidden")
-  heroContainer.classList.add("is-hidden")
-  event.preventDefault();
-})
-
-thxBtn.addEventListener("click", () => {
-  thxContainder.classList.remove("is-open");
-  heroContainer.classList.remove("is-hidden")
+function removeModalOpn () {
   document.body.classList.remove("modal-open")
-});
+};
+
+function toggleThanksMsg(event) {
+  refs.thxContainder.classList.toggle("is-open");
+  refs.heroContainer.classList.toggle("is-hidden")
+  refs.modal.classList.add("is-hidden")
+  event.preventDefault();
+}
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
